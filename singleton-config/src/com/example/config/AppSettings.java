8e44e68f17fd6b8b0ebe51b94cd7eb85ffdf299b
Siddharth Baleja <siddharthbaleja31@gmail.com>
2025-09-01 10:13:34 +0530

    Implementing Singleton Design Pattern

--- singleton-config/src/com/example/config/AppSettings.java
+++ singleton-config/src/com/example/config/AppSettings.java
@@ -11,11 +11,19 @@ import java.util.Properties;
  */
 public class AppSettings implements Serializable {
     private final Properties props = new Properties();
+    private static volatile AppSettings instance;
 
-    public AppSettings() { } // should not be public for true singleton
+    private AppSettings() { } // should not be public for true singleton
 
     public static AppSettings getInstance() {
-        return new AppSettings(); // returns a fresh instance (bug)
+        if(instance == null){
+            synchronized (AppSettings.class) {
+                if(instance == null){
+                    instance = new AppSettings();
+                }
+            }
+        }
+        return instance;
     }
 
     public void loadFromFile(Path file) {
